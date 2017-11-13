b4b094dd8ded63e223407333266ec57d02dabd37
ashleycano <acano6835@gmail.com>
2017-11-13 16:54:43 -0600

    added functionality to add movie and delete movie. working on delete and edit movie.

--- src/index.js
+++ src/index.js
@@ -14,18 +14,86 @@
 
     const {getMovies} = require('./api.js');
 
-    getMovies().then((movies) => {
-        console.log('Here are all the movies:');
-            let movieHTML = "";
-        movies.forEach(({title, rating, id}) => {
-            console.log()
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
-            movieHTML += `<div>id#${id} - ${title} - rating: ${rating}</div>`;
-            $("#movieData").html(movieHTML);
-        });
-    }).catch((error) => {
-        alert('Oh no! Something went wrong.\nCheck the console for details.')
-        console.log(error);
+   function updateMovies() {
+       getMovies().then((movies) => {
+           console.log('Here are all the movies:');
+           let movieHTML = "";
+           let currentMoviesHTML = "";
+           movies.forEach(({title, rating, id}) => {
+               console.log(`id#${id} - ${title} - rating: ${rating}`);
+               movieHTML += `<div>id#${id} - ${title} - rating: ${rating}`;
+               movieHTML += `<input id="checkBox" type="checkbox"></div>`;
+               $("#movieData").html(movieHTML);
+               currentMoviesHTML += `<option value=${id} class="value">${title}</option>`;
+               $("#movies").html(currentMoviesHTML);
+               $("#movieToEdit").val($("#movies option:selected").text());
+           });
+       }).catch((error) => {
+           alert('Oh no! Something went wrong.\nCheck the console for details.');
+           console.log(error);
+       });
+   }
+
+   updateMovies();
+
+    $('#submitMovie').click(function(e) {
+        e.preventDefault();
+        const newMovie = {
+           title: $('#title').val(),
+           rating: $('#rating').val()
+       };
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        };
+        fetch('/api/movies', options)
+            .then(() => console.log('post was created successfully!'))
+            .catch(() => console.log('Error on post'));
+        updateMovies();
+    });
+
+    $('#movies').change(function() {
+        $("#movieToEdit").val($("#movies option:selected").text());
+
+    });
+
+    $('#editMovie').click(function(e) {
+        e.preventDefault();
+        const newMovie = {
+            title: $('#title').val(),
+            rating: $('#rating').val()
+        };
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        };
+        fetch('/api/movies', options)
+            .then(() => console.log('post was created successfully!'))
+            .catch(() => console.log('Error on post'));
+        updateMovies();
+    });
+
+
+
+
+    $('#deleteMovie').click(function(e) {
+       e.preventDefault();
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        fetch('/api/movies/9', options)
+            .then(() => console.log('delete was created successfully!'))
+            .catch(() => console.log('Error on post'));
+        updateMovies();
     });
 
 // })
