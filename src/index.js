c2a965f2512710727f8d0e28d18c2bcab5465d03
Juan Candia <jcandia2@yahoo.com>
2017-11-13 12:29:34 -0600

    added movies to display on html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+// $(document).ready(function () {
+//     "use strict";
+    /**
+     * es6 modules and imports
+     */
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
+    // import sayHello from './hello';
+    // sayHello('World');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+    /**
+     * require style imports
+     */
+    const $ = require('jquery');
+
+    const {getMovies} = require('./api.js');
+
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+            let movieHTML = "";
+        movies.forEach(({title, rating, id}) => {
+            console.log()
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            movieHTML += `<div>id#${id} - ${title} - rating: ${rating}</div>`;
+            $("#movieData").html(movieHTML);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+
+// })
