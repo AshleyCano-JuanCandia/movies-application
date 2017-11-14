cef28696869f0c3c4afa8e74efe21a633fa58a72
Juan Candia <jcandia2@yahoo.com>
2017-11-14 13:56:18 -0600

    slight modification to css

--- src/index.js
+++ src/index.js
@@ -1,129 +1,130 @@
 // $(document).ready(function () {
 //     "use strict";
-    /**
-     * es6 modules and imports
-     */
+/**
+ * es6 modules and imports
+ */
 
-    // import sayHello from './hello';
-    // sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
-    /**
-     * require style imports
-     */
-    const $ = require('jquery');
+/**
+ * require style imports
+ */
+const $ = require('jquery');
 
-    const {getMovies} = require('./api.js');
+const {getMovies} = require('./api.js');
 
-    let movieData = {};
+let movieData = {};
 
-    let movieDataObject = {};
+let movieDataObject = {};
 
-    function getMovieData(movie) {
+function getMovieData(movie) {
 
-    }
-
-
-   function updateMovies() {
-       getMovies().then((movies) => {
-           console.log('Here are all the movies:');
-           let movieHTML = "";
-           let currentMoviesHTML = "";
-           movies.forEach(({title, rating, id}) => {
-               $.get(`http://www.omdbapi.com/?t=${title}&apikey=f9b07338&`, {
-
-               }).done(function (data) {
-                   console.group(title);
-                   movieData = {
-                       poster: data.Poster,
-                       director: data.Director,
-                       year: data.Year
-                   };
-
-                   console.log(movieData);
-
-                   console.log(`id#${id} - ${title} - rating: ${rating}`);
-                   console.log("data= ", movieData);
-                   movieHTML += `<div class="displayBox" style="background-image: url('${movieData.poster.trim()}')">`;
-                   movieHTML += `${title} <div class="smallerFont">${movieData.director}, ${movieData.year} <div class="smallerFont">rating: ${rating}</div></div></div>`;
-                   $("#movieData").html(movieHTML);
-                   currentMoviesHTML += `<option value=${id} class="value">${title}</option>`;
-                   $("#movies").html(currentMoviesHTML);
-                   $("#movieToEdit").val($("#movies option:selected").text());
-                   console.groupEnd(title);
-
-               }).fail(function () {
-                   console.log("error on request");
-               });
+}
 
 
+function updateMovies() {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        let movieHTML = "";
+        let currentMoviesHTML = "";
+        movies.forEach(({title, rating, id}) => {
+            $.get(`http://www.omdbapi.com/?t=${title}&apikey=f9b07338&`, {}).done(function (data) {
+                console.group(title);
+                movieData = {
+                    poster: data.Poster,
+                    director: data.Director,
+                    year: data.Year
+                };
 
-           });
-       }).catch((error) => {
-           alert('Oh no! Something went wrong.\nCheck the console for details.');
-           console.log(error);
-       });
-   }
+                console.log(movieData);
 
-   updateMovies();
+                console.log(`id#${id} - ${title} - rating: ${rating}`);
+                console.log("data= ", movieData);
+                movieHTML += `<div class="displayBox" style="background-image: url('${movieData.poster.trim()}')">`;
+                movieHTML += `${title} <div class="smallerFont">${movieData.director}, ${movieData.year} <div class="smallerFont">rating: ${rating}</div></div></div>`;
+                $("#movieData").html(movieHTML);
+                currentMoviesHTML += `<option value=${id} class="value">${title}</option>`;
+                $("#movies").html(currentMoviesHTML);
+                $("#movieToEdit").val($("#movies option:selected").text());
+                console.groupEnd(title);
 
-    $('#addMovie').click(function(e) {
-        e.preventDefault();
-        const newMovie = {
-           title: $('#title').val(),
-           rating: $('#rating').val()
-       };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newMovie),
-        };
-        fetch('/api/movies', options)
-            .then(() => console.log('post was created successfully!'))
-            .catch(() => console.log('Error on post'));
-        updateMovies();
+            }).fail(function () {
+                console.log("error on request");
+            });
+
+
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
     });
+}
 
-    $('#movies').change(function() {
-        $("#movieToEdit").val($("#movies option:selected").text());
+updateMovies();
 
+$('#addMovie').click(function (e) {
+    e.preventDefault();
+    const newMovie = {
+        title: $('#title').val(),
+        rating: $('#rating').val()
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch('/api/movies', options)
+        .then(() => console.log('post was created successfully!'))
+        .catch(() => console.log('Error on post'));
+    updateMovies();
+});
+
+$('#movies').change(function () {
+    $("#movieToEdit").val($("#movies option:selected").text());
+
+});
+
+$('#editMovie').click(function (e) {
+    e.preventDefault();
+    const newMovie = {
+        title: $('#title').val(),
+        rating: $('#rating').val()
+    };
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch('/api/movies', options)
+        .then(() => console.log('post was created successfully!'))
+        .catch(() => console.log('Error on post'));
+    updateMovies();
+});
+
+$(function () {
+    $("input[type=image]").click(function () {
+        $(this).addClass('selected');
     });
-
-    $('#editMovie').click(function(e) {
-        e.preventDefault();
-        const newMovie = {
-            title: $('#title').val(),
-            rating: $('#rating').val()
-        };
-        const options = {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newMovie),
-        };
-        fetch('/api/movies', options)
-            .then(() => console.log('post was created successfully!'))
-            .catch(() => console.log('Error on post'));
-        updateMovies();
-    });
+});
 
 
-
-
-    $('#deleteMovie').click(function(e) {
-       e.preventDefault();
-        const options = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };
-        fetch('/api/movies/9', options)
-            .then(() => console.log('delete was created successfully!'))
-            .catch(() => console.log('Error on post'));
-        updateMovies();
-    });
+$('#deleteMovie').click(function (e) {
+    e.preventDefault();
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+    fetch('/api/movies/9', options)
+        .then(() => console.log('delete was created successfully!'))
+        .catch(() => console.log('Error on post'));
+    updateMovies();
+});
 
 // })
